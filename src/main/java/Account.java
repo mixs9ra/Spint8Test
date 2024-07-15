5871408de91c9b3e5c6611233c44d3cb51892ec9
Denis Belashev <mix1316@yandex.ru>
2024-07-15 23:24:52 +0300

    Добавлены файлы

---
+++ src/main/java/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        return name.length() >= 3 && name.length() <= 19 && !name.isBlank() && name.contains(" ") && !name.startsWith(" ") && !name.endsWith(" ");
+    }
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AccountTest {
+    private final String nameTest;
+    private final boolean expected;
+
+    public AccountTest(String nameTest, boolean expected) {
+        this.nameTest = nameTest;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: {0}, {1}")
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"Бильбо Беггинс", true},
+                {"РоккиБильбо", false},
+                {" Бильбо Сумкин", false},
+                {"Сумкин Бильбо ", false},
+                {"Би", false},
+                {"Бильбо СумкоПодсумкинБеггинс", false},
+                {"Gasjd; jajs'L", true},
+        };
+    }
+
+    @Test
+    public void checkNameToEmbossWithDifferentMeanings() {
+        Account account = new Account(nameTest);
+        boolean actual = account.checkNameToEmboss();
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+}
